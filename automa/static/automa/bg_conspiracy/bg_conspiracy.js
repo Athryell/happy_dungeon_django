9c324acac876e203a72097377dc4643e00711093
Athryell <aleprovvi@yahoo.it>
2022-04-05 16:39:33 +0200

    Added scoresheet conspiracy and minor fixes

--- automa/static/automa/bg_conspiracy/bg_conspiracy.js
+++ automa/static/automa/bg_conspiracy/bg_conspiracy.js
@@ -1,40 +1,77 @@
 gsap.registerPlugin(Flip)
-const randomizeOpponentBtn = document.querySelector('h3')
 const leaders = document.querySelectorAll('.leader-box')
 const confirmationBox = document.getElementById('confirmation-box')
 const confirmBtn = document.getElementById('yes')
 const goBackBtn = document.getElementById('no')
 const randomBtn = document.querySelector('button')
 const token = document.getElementById('replay-token')
+const headline = document.querySelector('.headline')
+const scoreBtn = document.querySelector('.score-btn')
+const leaderPoints = document.querySelectorAll('.leader-points')
+const playerPoints = document.querySelectorAll('.player-points')
+const leaderScore = document.querySelector('.leader-score')
+const playerScore = document.querySelector('.player-score')
+const scoreTable = document.querySelector('.score-box')
+let totalLeaderScore = 0
+let leaderFaction
+let activeTooltip
 
 confirmBtn.addEventListener('click', () => {
-    token.hidden = false
+    token.style.display = 'block'
+    scoreBtn.style.display = 'inline'
     confirmationBox.hidden = true
+    headline.innerHTML = `Leader ${leaderFaction}`
+})
+
+scoreBtn.addEventListener('click', () => {
+    scoreTable.style.display = 'block'
+    scoreBtn.style.display = 'none'
+    // Reset inputs
+    leaderPoints.forEach(lp => {
+        lp.value = ''  
+    })
+    playerPoints.forEach(pp => {
+        pp.value = ''  
+    })
 })
 
 goBackBtn.addEventListener('click', () => {
+
+    gsap.set(leaders, { clearProps: "all" })
     leaders.forEach(l => {
-        // Reset styles
         l.hidden = false
-        l.style.opacity = 1
-        //TODO: Just fix this hardcoded shit
-        l.style.minHeight = '230px'
-        l.style.maxWidth = '15%'
-        l.style.minWidth = '170px'
-        l.style.margin = '0.5rem'
-        l.style.transform = "rotateY(0deg)"
+        l.removeEventListener('mouseenter', activateTooltip)
+        l.removeEventListener('mouseleave', inactivateTooltip)
     })
 
     confirmationBox.hidden = true
     randomBtn.style.visibility = 'visible'
 })
 
+// TOOLTIP
 leaders.forEach(leader => {
     leader.addEventListener('click', () => {
         hideCards(leader)
+        leaderFaction = leader.dataset.faction
+        activeTooltip = leader.querySelectorAll('.tooltip')
+        leader.addEventListener('mouseenter', activateTooltip)
+        leader.addEventListener('mouseleave', inactivateTooltip)
     })
 })
 
+function activateTooltip(){
+    activeTooltip.forEach(t => {
+        t.style.display = 'block'
+    })
+}
+
+function inactivateTooltip(){
+    activeTooltip.forEach(t => {
+        t.style.display = 'none'
+    })
+}
+
+// HIDE CARDS ON SELECTION
 function hideCards(leader){
     const cardsToHide = []
         const tl = gsap.timeline()
@@ -51,6 +88,7 @@ function hideCards(leader){
         tl.to(leaders, {duration: 0.5, margin:0 }, "shrink")
         tl.to(cardsToHide, {hidden: true}, "end")
         tl.to(confirmationBox, {hidden: false}, "end")
+        
 }
 
 // Choose random card
@@ -58,19 +96,47 @@ randomBtn.addEventListener('click', () => {
     randomBtn.style.visibility = 'hidden'
     const randInt = Math.floor(Math.random() * leaders.length)
 
-    const tl = gsap.timeline()
-    tl.to(leaders, {duration: 1, rotationY: 180, stagger: 0.2, ease: "back.out(2)"})
-    tl.to(leaders[randInt], {duration: 1, rotationY: 0, ease: "back.out(2)"})
-    tl.call(hideCards, [leaders[randInt]])
+    const tlRand = gsap.timeline()
+    tlRand.to(leaders, {duration: 1, rotationY: 180, stagger: 0.2, ease: "back.out(2)"})
+    tlRand.to(leaders[randInt], {duration: 1, rotationY: 0, ease: "back.out(2)"})
+    tlRand.call(hideCards, [leaders[randInt]])
+    leaderFaction = leaders[randInt].dataset.faction
 })
 
 // Token
 token.addEventListener('click', () => {
-    if (token.classList.contains('faded')){
-        token.classList.remove('faded')
-        token.classList.add('ready')
-    } else {
-        token.classList.remove('ready')
-        token.classList.add('faded')
-    }
+    token.classList.toggle('active')
+})
+
+// Score
+leaderPoints.forEach(lp => {
+    lp.addEventListener('input', () => {
+        let res = 0
+
+        leaderPoints.forEach(lPnts => {
+            let value = parseInt(lPnts.value)
+            if (isNaN(value))
+                return
+
+            res += value
+        })  
+
+        leaderScore.innerHTML = res
+    })  
+})
+
+playerPoints.forEach(pp => {
+    pp.addEventListener('input', () => {
+        let res = 0
+
+        playerPoints.forEach(pPnts => {
+            let value = parseInt(pPnts.value)
+            if (isNaN(value))
+                return
+
+            res += value
+        })  
+
+        playerScore.innerHTML = res
+    })  
 })
