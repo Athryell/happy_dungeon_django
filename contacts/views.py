a828b923585c0ab6c7e33adcf9dd838af06e995d
Athryell <aleprovvi@yahoo.it>
2022-07-14 15:47:56 +0200

    Changed contact form, now has captcha

--- contacts/views.py
+++ contacts/views.py
@@ -3,21 +3,21 @@ from django.core.mail import send_mail
 from django.conf import settings
 
 def contact(request):
-    if request.method == 'POST':
-        # if request.method == 'POST' and email and name:
-        #     send_mail(subject, content, settings.EMAIL_HOST_USER, ['kike1996@hotmail.com'], fail_silently=False)
-        message_name = request.POST['message-name']
-        message_email = request.POST['message-email']
-        message_text = request.POST['message-text']
+#     if request.method == 'POST':
+#         # if request.method == 'POST' and email and name:
+#         #     send_mail(subject, content, settings.EMAIL_HOST_USER, ['kike1996@hotmail.com'], fail_silently=False)
+#         message_name = request.POST['message-name']
+#         message_email = request.POST['message-email']
+#         message_text = request.POST['message-text']
 
-        subject = f'Sito Happy Dungeon - {message_name}'
-        content = f'Name: {message_name} \n - Email: {message_email} \n - Content: {message_text}'
+#         subject = f'Sito Happy Dungeon - {message_name}'
+#         content = f'Name: {message_name} \n - Email: {message_email} \n - Content: {message_text}'
 
-        send_mail(subject, content, settings.EMAIL_HOST_USER, ['happy_dungeon@hotmail.com'])
+#         send_mail(subject, content, settings.EMAIL_HOST_USER, ['happy_dungeon@hotmail.com'])
 
-        return render(request, 'contact/contact.html', {
-            'message_name': message_name
-        })
-    else:
-        return render(request, 'contact/contact.html')
+#         return render(request, 'contact/contact.html', {
+#             'message_name': message_name
+#         })
+#     else:
+    return render(request, 'contact/contact.html')
 
